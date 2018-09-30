839ea99a93126b8ce982b4c13c2330758d18e57b
Ben Glasser <ben.glasser@oracle.com>
2018-09-30 15:34:20 -0600

    add react router

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,11 +22,42 @@ import "phoenix_html"
 
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 class HelloReact extends React.Component {
     render() {
-        return (<h1>Hello React!</h1>)
+        return (
+            <Router>
+                <div>
+                <Route exact path="/" component={Home}/>
+            <Route path="/login" component={Login}/>
+            </div>
+            </Router>
+        )
     }
 }
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello React!</h1>
+        <Link to="/login">Login</Link>
+            </div>
+    )
+    }
+}
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>Hello Boring Login Page!</h1>
+        <Link to="/">Home</Link>
+            </div>
+    )
+    }
+}
+
 ReactDOM.render(
 <HelloReact/>,
     document.getElementById("hello-react")
