62961e1383fd69d2953a9dfac4524833e087a14b
Ben Glasser <ben.glasser@oracle.com>
2018-10-04 21:31:08 -0600

    refactor into class

--- assets/js/debit-table.js
+++ assets/js/debit-table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from "axios/index"
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -36,50 +36,48 @@ const styles = theme => ({
 
 
 
-function DebitTable(props) {
-    const { classes } = props;
-    function deleteHandler() {
+class DebitTable extends Component {
+     deleteHandler() {
         alert('delete')
     }
 
-    return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow className={classes.cell}>
-                        <TableCell className={classes.cell}>Item</TableCell>
-                        <TableCell className={classes.cell} >Category</TableCell>
-                        <TableCell className={classes.cell} numeric>Amount $</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.data.map(row => {
-                        return (
-                            <TableRow key={row[0]}>
-                                <TableCell className={classes.cell}>{row[1]}</TableCell>
-                                <TableCell className={classes.cell}>{row[2]}</TableCell>
-                                <TableCell className={classes.cell} numeric>{row[3]}</TableCell>
-                                <TableCell className={classes.cell} numeric>
-                                    <IconButton className={classes.button}
-                                                aria-label="Delete"
-                                                onClick={() => {
-                                                    alert('are you sure you want to delete "' + row[1] + '"')
-                                                    axios.delete('http://localhost:4000/json/debits/' + row[0])
-                                                }}>
-                                        <DeleteForeverIcon className={classes.icon} />
-                                    </IconButton>
-                                </TableCell>
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+    render() {
+        const { classes } = this.props;
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow className={classes.cell}>
+                            <TableCell className={classes.cell}>Item</TableCell>
+                            <TableCell className={classes.cell}>Category</TableCell>
+                            <TableCell className={classes.cell} numeric>Amount $</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.data.map(row => {
+                            return (
+                                <TableRow key={row[0]}>
+                                    <TableCell className={classes.cell}>{row[1]}</TableCell>
+                                    <TableCell className={classes.cell}>{row[2]}</TableCell>
+                                    <TableCell className={classes.cell} numeric>{row[3]}</TableCell>
+                                    <TableCell className={classes.cell} numeric>
+                                        <IconButton className={classes.button}
+                                                    aria-label="Delete"
+                                                    onClick={() => {
+                                                        alert('are you sure you want to delete "' + row[1] + '"')
+                                                        axios.delete('http://localhost:4000/json/debits/' + row[0])
+                                                    }}>
+                                            <DeleteForeverIcon className={classes.icon}/>
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
-
-DebitTable.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(DebitTable);
